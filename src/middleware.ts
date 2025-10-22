202bb825dfb1ecc8e3556bad2a232ca16ebcc4c5
sumit <rajputsumit6859@gmail.com>
2025-10-22 18:29:05 +0530

    update home page redirection

--- src/middleware.ts
+++ src/middleware.ts
@@ -54,14 +54,12 @@ export function middleware(request: NextRequest) {
 
 
   if (isHome && !isPublicRoute) {
-    debugger
     // return NextResponse.next();
-    return NextResponse.redirect(new URL('/waitlist', url));
+    return NextResponse.redirect(new URL('/home', url));
   }
 
 
   if (isPrivate(nextUrl.pathname) || isPrivateRoute) {
-
     if (token?.value === undefined) {
       const redirectUrl = request.nextUrl.clone()
       redirectUrl.pathname = '/login'
@@ -70,9 +68,7 @@ export function middleware(request: NextRequest) {
 
     }
   }
-
   if (!isPublicRoute && token?.value === undefined && nextUrl.pathname !== "/waitlist/subscriber") {
-    debugger
     const loginUrl = new URL('/login', url)
     loginUrl.searchParams.set('redirect', nextUrl.pathname)
     return NextResponse.redirect(new URL('/login', url));
@@ -127,8 +123,7 @@ export const config = {
     '/unauthorized',
     '/property/:path*',
     '/start-process/:path*',
-    '/waitlist',
-    '/waitlist/subscriber',
+    // '/waitlist/subscriber',
   ],
 };
 

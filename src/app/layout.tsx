bdcf01995df54e8f6c2b917807122ade8ca6c7ed
Abhradip Paul <abhradipserampore@gmail.com>
2025-12-16 18:26:23 +0530

    Add updated source code

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,4 +1,3 @@
-
 // import { Metadata } from 'next';
 // import Script from 'next/script';
 // import NextTopLoader from 'nextjs-toploader';
@@ -52,7 +51,6 @@
 // src/app/layout.tsx
 // src/app/layout.tsx
 
-
 // import Script from 'next/script';
 // import { metadata as md } from './metadata';
 // import { satoshi } from '../utils/fonts';
@@ -97,7 +95,6 @@
 //   );
 // }
 
-
 // src/app/layout.tsx
 import Script from 'next/script';
 import { metadata as md } from './metadata';
@@ -109,18 +106,35 @@ export const metadata = {
   description: md.description,
 };
 
-export default function RootLayout({ children }: { children: React.ReactNode }) {
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
   return (
     <html
-      lang="en"
-      className="min-h-full overflow-x-hidden"  // block horizontal scroll at the root
+      lang='en'
+      className='min-h-full overflow-x-hidden' // block horizontal scroll at the root
       suppressHydrationWarning
     >
       <head>
-        <meta name="viewport" content="width=device-width, initial-scale=1" />
-        <Script src="https://widget.cloudinary.com/v2.0/global/all.js" />
+        {/* Google Tag Manager */}
         <script
-          key="google-maps"
+          dangerouslySetInnerHTML={{
+            __html: `
+              (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+              new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+              j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+              'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+              })(window,document,'script','dataLayer','GTM-5WD3TKNN');
+            `,
+          }}
+        />
+        {/* End Google Tag Manager */}
+        <meta name='viewport' content='width=device-width, initial-scale=1' />
+        <Script src='https://widget.cloudinary.com/v2.0/global/all.js' />
+        <script
+          key='google-maps'
           src={`https://maps.googleapis.com/maps/api/js?key=${process.env.NEXT_PUBLIC_GOOGLE_MAPS_API_KEY}&libraries=places`}
           async
           defer
@@ -129,12 +143,20 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
 
       <body
         style={satoshi.style}
-        className="min-h-screen flex flex-col font-satoshi scroll-smooth overflow-x-hidden" // let the document handle vertical scroll
+        className='font-satoshi flex min-h-screen flex-col overflow-x-hidden scroll-smooth' // let the document handle vertical scroll
       >
+        {/* Google Tag Manager (noscript) */}
+        <noscript>
+          <iframe
+            src='https://www.googletagmanager.com/ns.html?id=GTM-5WD3TKNN'
+            height='0'
+            width='0'
+            style={{ display: 'none', visibility: 'hidden' }}
+            title='Google Tag Manager'
+          />
+        </noscript>
         <ClientRoot>
-          <main className="flex-1">
-            {children}
-          </main>
+          <main className='flex-1'>{children}</main>
         </ClientRoot>
       </body>
     </html>

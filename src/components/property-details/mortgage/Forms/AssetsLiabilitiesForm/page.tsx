5cd6d277b81438127f3f6d170fa9e4732778a80c
Abhradip Paul <abhradipserampore@gmail.com>
2026-01-02 14:16:53 +0530

    Add updated image location

--- src/components/property-details/mortgage/Forms/AssetsLiabilitiesForm/page.tsx
+++ src/components/property-details/mortgage/Forms/AssetsLiabilitiesForm/page.tsx
@@ -288,7 +288,7 @@ const AssetsLiabilitiesForm = ({formData, setFormData}:IProps) => {
                 onClick={() => open()} 
                 disabled={!ready} 
                 className="ml-4">
-                <Image src="/plaid_logo.svg" alt="Plaid" width={72} height={72} />
+                <Image src="/assets/plaid_logo.svg" alt="Plaid" width={72} height={72} />
               </button>
             </div>
           </div>

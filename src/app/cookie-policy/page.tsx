142e1780e94a9d49c7f66761a8e525e15eb9319b
Arpita Panda <arpitapanda261@gmail.com>
2026-01-24 18:27:33 +0530

    Add footer links

---
+++ src/app/cookie-policy/page.tsx
@@ -0,0 +1,5 @@
+import CookiePolicy from '@/components/cookie-policy/cookie-policy';
+
+export default function CookiePolicyPage() {
+  return <CookiePolicy />;
+}
---
+++ src/app/offer-terms/page.tsx
@@ -0,0 +1,5 @@
+import OfferTerms from '@/components/offer-terms/offer-terms';
+
+export default function OfferTermsPage() {
+  return <OfferTerms />;
+}
